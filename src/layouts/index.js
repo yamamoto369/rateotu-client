3f1eead830e82175211e7d2bd6e3bd1d87ab76bb
Edin <yamamoto.nakamoto@gmail.com>
2022-04-28 12:23:15 +0200

    Updated HTML title tag

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -66,7 +66,7 @@ class IndexLayout extends React.PureComponent {
     return (
       <Fragment>
         <Helmet
-          titleTemplate="Milliways | The Restaurant at the End of the Universe"
+          titleTemplate="Milliways | The Restaurant at the End of the Universe | Customer Client"
           title="Milliways - The Restaurant at the End of the Universe"
         />
         {BootstrappedLayout()}
